76734a69f3a75fee56a1ee0a033524415ff1f41d
Dustin Einglett <x13thangelx@gmail.com>
2012-03-13 18:03:34 -0500

    Convert D2 into a common repo finally

--- cm.mk
+++ cm.mk
@@ -11,15 +11,9 @@ $(call inherit-product, device/motorola/droid2/droid2.mk)
 $(call inherit-product, vendor/motorola/droid2/droid2-vendor.mk)
 
 ## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := droid2
 PRODUCT_NAME := cm_droid2
 PRODUCT_BRAND := verizon
-PRODUCT_DEVICE := droid2
-PRODUCT_MODEL := Droid2
+PRODUCT_MODEL := DROID2
 PRODUCT_MANUFACTURER := Motorola
-PRODUCT_SFX := vzw
 
-# Release name and versioning
-PRODUCT_RELEASE_NAME := Droid2
-
-UTC_DATE := $(shell date +%s)
-DATE := $(shell date +%Y%m%d)
